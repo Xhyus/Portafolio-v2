769d6cfe404255812165dfa3699794e25916f9a9
ignacio González <ignaciogonzalez1609@gmail.com>
2023-07-30 20:52:13 -0400

    Small fix

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,28 +1,28 @@
-import { Stack } from '@chakra-ui/react'
-import experiencias from '../static/experiencias'
-import SubTitulo from './generic_components/SubTitulo'
-import PresentacionExperiencia from './generic_components/PresentacionExperiencia'
+import { Stack } from "@chakra-ui/react";
+import experiencias from "../static/experiencias";
+import SubTitulo from "./generic_components/SubTitulo";
+import PresentacionExperiencia from "./generic_components/PresentacionExperiencia";
 
 const Experiencia = () => {
-    return (
-        <Stack justify={"center"} py={10} w={'full'} id={"Experiencias"}>
-            <SubTitulo type="h2" size="2xl" data="EXPERIENCIAS" />
-            {experiencias.map((experiencia, index) => {
-                return (
-                    <PresentacionExperiencia
-                        key={index}
-                        periodo={experiencia.periodo}
-                        empresa={experiencia.empresa}
-                        detalle={experiencia.descripcion}
-                        experiencia={experiencia.experiencia}
-                        nombre={experiencia.nombre}
-                        correo={experiencia.correo}
-                        referencia={experiencia.referencia}
-                    />
-                )
-            })}
-        </Stack>
-    )
-}
+  return (
+    <Stack justify={"center"} py={10} w={"full"} id={"Experiencias"}>
+      <SubTitulo type="h2" size="2xl" data="EXPERIENCIAS" />
+      {experiencias.map((experiencia) => {
+        return (
+          <PresentacionExperiencia
+            key={experiencia.code}
+            periodo={experiencia.periodo}
+            empresa={experiencia.empresa}
+            detalle={experiencia.descripcion}
+            experiencia={experiencia.experiencia}
+            nombre={experiencia.nombre}
+            correo={experiencia.correo}
+            referencia={experiencia.referencia}
+          />
+        );
+      })}
+    </Stack>
+  );
+};
 
-export default Experiencia
+export default Experiencia;
--- src/static/experiencias.js
+++ src/static/experiencias.js
@@ -1,5 +1,6 @@
 const experiencias = [
   {
+    code: 7,
     empresa: "MasterBase®",
     descripcion:
       "Desarrollo y mantención de multiples microservicios para la plataforma de MasterBase®. Se desarrollaron utilizando tecnologias orientadas en Javascript.",
@@ -8,6 +9,7 @@ const experiencias = [
     referencia: false,
   },
   {
+    code: 6,
     empresa: "Estudio Fragua",
     descripcion:
       "Proyecto de título enfocado en el desarrollo de una plataforma de cotizaciones. Este proyecto se desarrolló con el stack MERN usando Next.JS y Chakra UI.",
@@ -19,6 +21,7 @@ const experiencias = [
     correo: "cnovoa@pryx.cl",
   },
   {
+    code: 5,
     empresa: "Universidad del Bío Bío",
     descripcion:
       "Ayudantías para Ingeniería de Ejecución e Informática y para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocan en el aprendizaje del stack MERN.",
@@ -30,6 +33,7 @@ const experiencias = [
     correo: "asegura@ubiobio.cl",
   },
   {
+    code: 4,
     empresa: "Universidad del Bío Bío",
     descripcion:
       "Ayudantías para Ingeniería Civil Informática en la asignatura de Ingeniería de Software, las clases se enfocaron en el uso del patron de desarrollo MVC mediante PHP, HTML y CSS.",
@@ -41,6 +45,7 @@ const experiencias = [
     correo: "asegura@ubiobio.cl",
   },
   {
+    code: 3,
     empresa: "Pryx",
     descripcion:
       "Practica Profesional II enfocada en el desarrollando páginas web con Wordpress, CSS y PHP. Además se desarrollaron aplicaciones móviles con React Native y CSS",
@@ -52,6 +57,7 @@ const experiencias = [
     correo: "cnovoa@pryx.cl",
   },
   {
+    code: 2,
     empresa: "Pryx",
     descripcion:
       "Practica Profesional I enfocada en el desarrollo de páginas web con Wordpress y CSS. Además de un sistema de gestion de inventario desarrollado con Django.",
@@ -63,6 +69,7 @@ const experiencias = [
     correo: "cnovoa@pryx.cl",
   },
   {
+    code: 1,
     empresa: "MIBO",
     descripcion:
       "Apoyo en certificaciones de empresas para el uso de boletas electronicas con SII mediante MIBO.",
